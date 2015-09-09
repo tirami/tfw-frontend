7b1d9864034ba1d6d4960464a426f3ea5b8048b6
Rory Gianni <giannichan@gmail.com>
2015-09-09 18:15:26 +0100

    adds fns for drawing word cloud and bar chat to directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,10 +3,61 @@
 /* Directives */
 var udadisiDirectives = angular.module('udadisiDirectives', []);
 
-udadisiDirectives.directive('wordcloud', [
-  function() {
-    return { restrict: 'A', scope: {}, template: 'my word cloud',
-      link: function(scope, element, attrs) { }
-    }
+var barChart = function (scope, element, attrs) {
+  var data = attrs.chartData.split(','); //e.g.  <chart chart-data="40,100,80,15,25,60,10"></chart>
+  //in D3, any selection[0] contains the group
+  //selection[0][0] is the DOM node
+  //but we won't need that this time
+  var chart = d3.select(element[0]);
+  //to our original directive markup bars-chart
+  //we add a div with out chart stling and bind each
+  //data entry to the chart
+  chart.append("div").attr("class", "chart")
+   .selectAll('div')
+   .data(data).enter().append("div")
+   .transition().ease("elastic")
+   .style("width", function(d) { return d + "%"; })
+   .text(function(d) { return d + "%"; });
+  //a little of magic: setting it's width based
+  //on the data value (d) 
+  //and text all with a smooth transition
+};
+
+var drawWordcloud = function(scope, element, attrs) {
+
+  var fill = d3.scale.category20();
+  var wordsArr = ["Hello", "world", "normally", "you", "want", "more", "words", "than", "this"];
+
+  var layout = d3.layout.cloud().size([500, 500]).words(wordsArr.map(
+    function(d) { return {text: d, size: 10 + Math.random() * 90, test: "haha"}; }))
+      .padding(5).rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .font("Impact").fontSize(function(d) { return d.size; })
+      .on("end", draw);
+
+  layout.start();
+
+  function draw(words) {
+    //console.log(element);
+    d3.select(element[0]).append("svg")
+    .attr("width", layout.size()[0])
+    .attr("height", layout.size()[1])
+    .append("g")
+    .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+    .selectAll("text")
+    .data(words)
+    .enter().append("text")
+    .style("font-size", function(d) { return d.size + "px"; })
+    .style("font-family", "Impact")
+    .style("fill", function(d, i) { return fill(i); })
+    .attr("text-anchor", "middle")
+    .attr("transform", function(d) {
+    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+    }).text(function(d) { return d.text; });
+  };
+}
+
+udadisiDirectives.directive('wordcloud', 
+  function($parse) {
+    return { restrict: 'E', scope: {}, replace: true, link: drawWordcloud }
   }
-]);
+);
