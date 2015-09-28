3fcf4fbb330f26e94e5daa0bea74bb96de7a203a
Rory Gianni <giannichan@gmail.com>
2015-09-28 17:35:44 +0100

    add globe projection and refactor directives

--- app/js/directives.js
+++ app/js/directives.js
@@ -5,13 +5,13 @@ var udadisiDirectives = angular.module('udadisiDirectives', []);
 
 udadisiDirectives.directive('wordcloud', 
   function($parse) {
-    return { restrict: 'E', scope: { trends: '=' }, link: drawWordcloud }
+    return { restrict: 'A', scope: { trends: '=' }, link: drawWordcloud }
   }
 );
 
 udadisiDirectives.directive('timespan', 
   function($parse) {
-    return { restrict: 'E', scope: { selectStart: '=', location: '=', interval: '=', start: '=', end: '=', updateFn: '=' }, link: setTimespan }
+    return { restrict: 'A', scope: { selectStart: '=', location: '=', interval: '=', start: '=', end: '=', updateFn: '=' }, link: setTimespan }
   }
 );
 
@@ -20,6 +20,45 @@ udadisiDirectives.directive('locationToggle',
     return { restrict: 'C', scope: { selectStart: '=', location: '=', interval: '=', updateFn: '=' }, link: setLocation }
   }
 );
+
+udadisiDirectives.directive('mapProjection', 
+  function($parse) {
+    return { restrict: 'A', scope: { }, link: drawMap }
+  }
+);
+
+var drawMap = function(scope,element,attrs){
+  //var width = 960, height = 480;
+  var bbox = d3.select(element[0]).node().getBoundingClientRect();
+  var width = bbox.width, height = bbox.width/2;
+  console.log(bbox);
+
+  var projection = d3.geo.equirectangular().scale(153).translate([width / 2, height / 2]).precision(.1);
+
+  var path = d3.geo.path().projection(projection);
+
+  var graticule = d3.geo.graticule();
+
+  var svg = d3.select(element[0]).append("svg").attr("width", width).attr("height", height);
+
+  svg.append("path").datum(graticule).attr("class", "graticule").attr("d", path);
+
+  d3.json("/app/world.json", function(error, world) {
+    if (error) throw error;
+
+    svg.insert("path", ".graticule")
+        .datum(topojson.feature(world, world.objects.land))
+        .attr("class", "land")
+        .attr("d", path);
+
+    svg.insert("path", ".graticule")
+        .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+        .attr("class", "boundary")
+        .attr("d", path);
+  });
+
+  d3.select(self.frameElement).style("height", height + "px");
+};
 
 var setLocation = function(scope, element, attrs) {
   element.on('click', function(event) {
