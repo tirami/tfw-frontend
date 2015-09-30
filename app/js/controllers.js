394517cdb037291fdc14de54a0fe3837b5dcf2b9
Rory Gianni <giannichan@gmail.com>
2015-09-30 11:49:25 +0100

    start refactoring controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,19 +1,30 @@
 'use strict';
 
 /* Controllers */
+var startOfToday = function(){
+  return Date.now() - (Date.now() % (24*60*60*1000));
+}
+
+var day   = 24*60*60*1000;
+var today = startOfToday(); // CHANGE TO startOfToday(); to get up to date info
 
 var udadisiControllers = angular.module('udadisiControllers', ['ngRoute']);
 
 udadisiControllers.controller('HomeCtrl', ['$scope', '$log', 'Trends', function($scope, $log, Trends) { 
-  $scope.getTrends = function(location){ Trends.query({ location: location }, function(data) {
+  
+  $scope.selectionStart = today-(1*day);
+  $scope.location = "all";
+  $scope.interval = 1;
+
+  $scope.getTrends = function(location, fromDate, interval){ Trends.query({ location: location, limit: 10, from: fromDate, interval: interval }, function(data) {
       $scope.trends = data;
     }, function(error){
-      //$log.log(error);
-      $scope.trendsMessage = "No trends received from remote server, using examples: "
+      $scope.trendsMessage = "No trends received from remote server, using examples: ";
       $scope.trends = [{"term":"solar","occurrences":442},{"term":"battery","occurrences":407}]; 
-    }); 
+    });
   };
-  $scope.getTrends('all');
+
+  $scope.getTrends($scope.location, new Date($scope.selectionStart).yyyymmdd(), $scope.interval);
 }]);
 
 udadisiControllers.controller('LocationsCtrl', ['$scope', '$routeParams',
@@ -25,10 +36,7 @@ udadisiControllers.controller('TrendsCtrl', ['$scope', '$routeParams',
 }]);
 
 udadisiControllers.controller('ExplorerCtrl', ['$scope', '$log', 'Trends', function($scope, $log, Trends) { 
-
-  var day   = 24*60*60*1000;
-  var today = 1440111600000; // CHANGE TO startOfToday(); to get up to date info
-  
+  //today = 1440111600000; // CHANGE TO startOfToday(); to get up to date info
   $scope.spanEnd   = today-1; //at 23:59:59
   $scope.spanStart = today-(7*day); //week before
   
@@ -46,7 +54,3 @@ udadisiControllers.controller('ExplorerCtrl', ['$scope', '$log', 'Trends', funct
 
   $scope.getTrends($scope.location, new Date($scope.selectionStart).yyyymmdd(), $scope.interval);
 }]);
-
-var startOfToday = function(){
-  return Date.now() - (Date.now() % (24*60*60*1000));
-}
