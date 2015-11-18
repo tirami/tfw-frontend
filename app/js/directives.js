bbaa6ef5192315c6f5be9246f166fa96f787b25a
Rory Gianni <giannichan@gmail.com>
2015-11-18 13:22:43 +0000

    adds base node visualisation

--- app/js/directives.js
+++ app/js/directives.js
@@ -52,14 +52,132 @@ udadisiDirectives.directive('timeSeries',
 
 udadisiDirectives.directive('nodeGraph', 
   function($parse) {
-    return { priority: 0, restrict: 'A', scope: { seriesData: '=' }, link: drawNodes }
+    return { priority: 0, restrict: 'A', scope: { trend: '=' }, link: drawNodes }
   }
 );
 
-var drawNodes = function(){
+var drawNodes = function(scope, element, attrs){
+  //trend.word_counts
+
+  var bbox = d3.select('#node-container').node().getBoundingClientRect();
+  var margin = {top: 10, right: 10, bottom: 10, left: 10};
+  var width = bbox.width - margin.left - margin.right;
+  var height = bbox.height - margin.top - margin.bottom;
+
+  var node, link, root;
+  var force = d3.layout.force()
+    .on("tick", tick)
+    .charge(function(d) { return d._children ? -d.size / 100 : -30; })
+    .linkDistance(function(d) { return d.target._children ? 100 : 50; })
+    .size([width+100, height+100]);
+
+  var vis = d3.select(element[0]).append("svg:svg").attr("width", width).attr("height", height);
+
+  var json = { "name": "flare", "children": 
+    [{"name": "analytics","children": [{ "name": "cluster", "children": [
+      {"name": "AgglomerativeCluster", "size": 3938},
+      {"name": "CommunityStructure", "size": 3812},
+      {"name": "HierarchicalCluster", "size": 16714},
+      {"name": "MergeEdge", "size": 743}
+    ]
+    }]}]};
+
+  root = json;
+  root.fixed = true;
+  root.x = width / 2;
+  root.y = height / 2;
+  update();
+  
+  function update() {
+    var nodes = flatten(root),
+        links = d3.layout.tree().links(nodes);
+
+    // Restart the force layout.
+    force
+        .nodes(nodes)
+        .links(links)
+        .start();
+
+    // Update the links…
+    link = vis.selectAll("line.link")
+        .data(links, function(d) { return d.target.id; });
+
+    // Enter any new links.
+    link.enter().insert("svg:line", ".node")
+        .attr("class", "link")
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    // Exit any old links.
+    link.exit().remove();
+
+    // Update the nodes…
+    node = vis.selectAll("circle.node")
+        .data(nodes, function(d) { return d.id; })
+        .style("fill", color);
+
+    node.transition()
+        .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; });
+
+    // Enter any new nodes.
+    node.enter().append("svg:circle")
+        .attr("class", "node")
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; })
+        .style("fill", color)
+        .on("click", click)
+        .call(force.drag);
+
+    // Exit any old nodes.
+    node.exit().remove();
+  }
+
+  function tick() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  }
+
+  // Color leaf nodes orange, and packages white or blue.
+  function color(d) {
+    return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
+  }
+
+  // Toggle children on click.
+  function click(d) {
+    if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    } else {
+      d.children = d._children;
+      d._children = null;
+    }
+    update();
+  }
+
+  // Returns a list of all nodes under the root.
+  function flatten(root) {
+    var nodes = [], i = 0;
+
+    function recurse(node) {
+      if (node.children) node.size = node.children.reduce(function(p, v) { return p + recurse(v); }, 0);
+      if (!node.id) node.id = ++i;
+      nodes.push(node);
+      return node.size;
+    }
+
+    root.size = recurse(root);
+    return nodes;
+  }
 
 };
-
 
 var drawTimeSeries = function(scope, element, attrs){
   //var trends = { term: "battery", timeseries: [100000:100, 100000:100] }
