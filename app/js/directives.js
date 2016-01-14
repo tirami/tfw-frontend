525e5e329fcd1a9053eae6b9fb93d6aa91784f0c
Rory Gianni <giannichan@gmail.com>
2016-01-14 09:42:41 +0000

    adds time string formatter for updated api

--- app/js/directives.js
+++ app/js/directives.js
@@ -644,10 +644,18 @@ var drawTreemap = function(scope, element, attrs){
 
 };
 
-
 Date.prototype.yyyymmdd = function() {
   var yyyy = this.getFullYear().toString();
   var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
   var dd  = this.getDate().toString();
   return yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]); // padding
+};
+
+Date.prototype.toTimeString = function() {
+  var yyyy = this.getFullYear().toString();
+  var month = (this.getMonth()+1).toString(); // getMonth() is zero-based
+  var day = this.getDate().toString();
+  var hh = this.getHours().toString();
+  var mm = this.getMinutes().toString();
+  return yyyy + (month[1]?month:"0"+month[0]) + (day[1]?day:"0"+day[0]) + (hh[1]?hh:"0"+hh[0]) + (mm[1]?mm:"0"+mm[0]); //zero padding
 };
