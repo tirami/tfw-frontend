664489817fbaba79005c35b4d39b62ff41a57038
Rory Gianni <giannichan@gmail.com>
2016-01-19 11:27:19 +0000

    shorten margin on timespan

--- app/js/directives.js
+++ app/js/directives.js
@@ -524,7 +524,7 @@ var drawBars = function (scope, element, attrs) {
 
 var setTimespan = function(scope, element, attrs) {
   var container = d3.select(element[0]),
-    margin = {top: 0, right: 95, bottom: 0, left: 40},
+    margin = {top: 0, right: 20, bottom: 0, left: 40},
     height = 50;
   var width = (container.node().offsetWidth) - margin.left - margin.right;
 
