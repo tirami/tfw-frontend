c7b51333e6d59f21063baa41db9d9517c0a09092
Rory Gianni <giannichan@gmail.com>
2016-02-03 12:59:54 +0000

    wrap bar terms in span

--- app/js/directives.js
+++ app/js/directives.js
@@ -598,7 +598,7 @@ var drawBars = function (scope, element, attrs) {
       .selectAll('div')
       .data(cleaned).enter().append("div")
       .style("width", function(d) { return (d[scope.property]/extents[1]*100) + "%"; })
-      .style("height", "1.8em")
+      .style("height", "1.8em").append("span")
       .text(function(d) { return d.term; });
    });
 };
